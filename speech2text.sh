64b71d05de2da2fdaa69694ec8fabcb369efddd6
Tanel Alumäe <alumae@gmail.com>
2018-08-23 08:36:34 +0300

    Some minor improvements

--- speech2text.sh
+++ speech2text.sh
@@ -8,29 +8,26 @@ txt=""
 trs=""
 ctm=""
 sbv=""
+srt=""
+with_compounds_ctm=""
 clean=true
-nthreads=""
-nnet2_online=true
 
 . $BASEDIR/utils/parse_options.sh || exit 1;
 
 if [ $# -ne 1 ]; then
   echo "Usage: speech2text [options] <audiofile>"
-  echo "Options:"
-  echo "  --nthreads <n>        # Use <n> threads in parallel for decoding"
-  echo "  --txt <txt-file>      # Put the result in a simple text file"
-  echo "  --trs <trs-file>      # Put the result in trs file (XML file for Transcriber)"
-  echo "  --ctm <ctm-file>      # Put the result in CTM file (one line pwer word with timing information)"
-  echo "  --sbv <sbv-file>      # Put the result in SBV file (subtitles for e.g. YouTube)"
+  echo "Options:"  
+  echo "  --nthreads <n>                   # Use <n> threads in parallel for decoding"
+  echo "  --txt <txt-file>                 # Put the result in a simple text file"
+  echo "  --trs <trs-file>                 # Put the result in trs file (XML file for Transcriber)"
+  echo "  --ctm <ctm-file>                 # Put the result in CTM file (one line pwer word with timing information)"
+  echo "  --sbv <sbv-file>                 # Put the result in SBV file (subtitles for e.g. YouTube)"
+  echo "  --srt <srt-file>                 # Put the result in SRT file (subtitles for e.g. VLC)"
+  echo "  --with-compounds-ctm <ctm-file>  # Put the result in CTM file (with compound break symbols)"
   echo "  --clean (true|false)  # Delete intermediate files generated during decoding (true by default)"
   exit 1;
 fi
 
-nthreads_arg=""
-if [ ! -z $nthreads ]; then
-  echo "Using $nthreads threads for decoding"
-  nthreads_arg="nthreads=$nthreads"
-fi
   
 cp -u $1 $BASEDIR/src-audio
 
@@ -38,9 +35,9 @@ filename=$(basename "$1")
 basename="${filename%.*}"
 
 
-(cd $BASEDIR; make $nthreads_arg build/output/$basename.{txt,trs,ctm,sbv} || exit 1; if $clean ; then make .$basename.clean; fi)
+(cd $BASEDIR; make $nthreads_arg build/output/$basename.{txt,trs,ctm,sbv,srt,with-compounds.ctm} || exit 1; if $clean ; then make .$basename.clean; fi)
 
-echo "Finished transcribing, result is in files $BASEDIR/build/output/${basename%.*}.{txt,trs,ctm,sbv}"
+echo "Finished transcribing, result is in files $BASEDIR/build/output/${basename%.*}.{txt,trs,ctm,sbv,srt,with-compounds.ctm}"
 
 if [ ! -z $txt ]; then
   cp $BASEDIR/build/output/${basename}.txt $txt
@@ -58,4 +55,13 @@ fi
 if [ ! -z $sbv ]; then
   cp $BASEDIR/build/output/${basename}.sbv $sbv
 fi
+
+if [ ! -z $srt ]; then
+  cp $BASEDIR/build/output/${basename}.srt $srt
+fi
+
+if [ ! -z $with_compounds_ctm ]; then
+  cp $BASEDIR/build/output/${basename}.with-compounds.ctm $with_compounds_ctm
+fi
+
 
