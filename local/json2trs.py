b03a80a82800459c8548740435ee269a01b6f2a0
Tanel Alumäe <alumae@gmail.com>
2018-11-14 16:10:58 +0200

    Fixed a Transcriber file format issue

--- local/json2trs.py
+++ local/json2trs.py
@@ -17,18 +17,19 @@ def print_footer():
   print('</Episode>')
   print('</Trans>')
     
+    
 def print_speakers(speakers):
   print("<Speakers>")
   i = 1
   for speaker_id, speaker in speakers.items():
     default_speaker_name = "K%02d" % i    
-    print('<Speaker id="%s" name="%s" check="no" dialect="native" accent="" scope="local" type="male"/>' % (speaker_id, speaker.get("name", default_speaker_name)))
+    print('<Speaker id="%s" name="%s" check="no" dialect="native" accent="" scope="local"/>' % (speaker["transcriber_id"], speaker.get("name", default_speaker_name)))
     i += 1
   print("</Speakers>")
   print('<Episode>')
         
 
-def print_sections(sections):
+def print_sections(sections, speakers):
   for section in sections:
     section_type = section["type"]     
     
@@ -36,12 +37,13 @@ def print_sections(sections):
       print('<Section type="%s" startTime="%0.3f" endTime="%0.3f">' % ("report", section["start"], section["end"]))
       for turn in section.get("turns", []):
       
-        print('<Turn speaker="%s" startTime="%0.3f" endTime="%0.3f">' % (turn["speaker"], turn["start"], turn["end"]))
+        print('<Turn speaker="%s" startTime="%0.3f" endTime="%0.3f">' % (speakers[turn["speaker"]]["transcriber_id"], turn["start"], turn["end"]))
         print(turn["transcript"])
         print('</Turn>')
       print('</Section>')
     elif section_type == "non-speech":
       print('<Section type="%s" startTime="%0.3f" endTime="%0.3f">' % ("filler", section["start"], section["end"]))
+      print('</Section>')
 
 parser = argparse.ArgumentParser("Converts JSON format to Transcriber trs")
 parser.add_argument('--fid', default="unknown", help="File id to be used in trs header")
@@ -51,8 +53,12 @@ args = parser.parse_args()
 
 trans = json.load(open(args.json))
 
+# Add speciial transcriber_id fields (Transcriber needs IDs to obey this format, it seems)
+for (i, speaker) in enumerate(trans["speakers"].values()):
+  speaker["transcriber_id"] = "spk%d" % (i + 1)
+
 print_header(args.fid)
 print_speakers(trans["speakers"])
-print_sections(trans["sections"])
+print_sections(trans["sections"], trans["speakers"])
 print_footer()
 
